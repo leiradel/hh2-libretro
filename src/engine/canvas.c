b1c56f448cc7344e67abf94e768e98fcafbf8382
Andre Leiradella <andre@leiradella.com>
2021-03-18 19:05:34 +0000

    Better name for the 16-bit color type and macros

--- src/engine/canvas.c
+++ src/engine/canvas.c
@@ -5,21 +5,21 @@
 
 #define TAG "CNV "
 
-// Image blit is coded for 16 bpp, make sure the build fails if hh2_Color does not have 16 bits
-typedef char hh2_staticAssertColorMustHave16Bits[sizeof(hh2_Color) == 2 ? 1 : -1];
+// Image blit is coded for 16 bpp, make sure the build fails if hh2_RGB565 does not have 16 bits
+typedef char hh2_staticAssertColorMustHave16Bits[sizeof(hh2_RGB565) == 2 ? 1 : -1];
 
 struct hh2_Canvas {
     unsigned width;
     unsigned height;
     size_t pitch; // in bytes
 
-    hh2_Color pixels[1];
+    hh2_RGB565 pixels[1];
 };
 
 hh2_Canvas hh2_createCanvas(unsigned const width, unsigned const height) {
     HH2_LOG(HH2_LOG_INFO, TAG "creating %u x %u canvas", width, height);
 
-    size_t const pitch = ((width + 3) & ~3) * sizeof(hh2_Color);
+    size_t const pitch = ((width + 3) & ~3) * sizeof(hh2_RGB565);
     size_t const size = pitch * height;
 
     HH2_LOG(HH2_LOG_DEBUG, TAG "canvas pitch is %zu bytes, data is %zu bytes", pitch, size);
@@ -56,21 +56,21 @@ size_t hh2_canvasPitch(hh2_Canvas const canvas) {
     return canvas->pitch;
 }
 
-void hh2_clearCanvas(hh2_Canvas const canvas, hh2_Color const color) {
+void hh2_clearCanvas(hh2_Canvas const canvas, hh2_RGB565 const color) {
     unsigned const width = canvas->width;
     unsigned const height = canvas->height;
     size_t const pitch = canvas->pitch;
-    hh2_Color* pixel = canvas->pixels;
+    hh2_RGB565* pixel = canvas->pixels;
 
     for (unsigned y = 0; y < height; y++) {
         for (unsigned x = 0; x < width; x++) {
             pixel[x] = color;
         }
 
-        pixel = (hh2_Color*)((uint8_t*)pixel + pitch);
+        pixel = (hh2_RGB565*)((uint8_t*)pixel + pitch);
     }
 }
 
-hh2_Color* hh2_canvasPixel(hh2_Canvas canvas, unsigned x, unsigned y) {
-    return (hh2_Color*)((uint8_t*)canvas->pixels + y * canvas->pitch) + x;
+hh2_RGB565* hh2_canvasPixel(hh2_Canvas canvas, unsigned x, unsigned y) {
+    return (hh2_RGB565*)((uint8_t*)canvas->pixels + y * canvas->pitch) + x;
 }
--- src/engine/canvas.h
+++ src/engine/canvas.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define HH2_COLOR(r, g, b) ((((hh2_Color)(r) << 8 | (hh2_Color)(b) >> 3) & 0xf81fU) | (((hh2_Color)(g) << 3) & 0x07e0U))
+#define HH2_COLOR_RGB565(r, g, b) ((((hh2_RGB565)(r) << 8 | (hh2_RGB565)(b) >> 3) & 0xf81fU) | (((hh2_RGB565)(g) << 3) & 0x07e0U))
 
-typedef uint16_t hh2_Color;
+typedef uint16_t hh2_RGB565;
 typedef struct hh2_Canvas* hh2_Canvas;
 
 hh2_Canvas hh2_createCanvas(unsigned width, unsigned height);
@@ -16,8 +16,8 @@ unsigned hh2_canvasWidth(hh2_Canvas canvas);
 unsigned hh2_canvasHeight(hh2_Canvas canvas);
 size_t hh2_canvasPitch(hh2_Canvas canvas);
 
-void hh2_clearCanvas(hh2_Canvas canvas, hh2_Color color);
+void hh2_clearCanvas(hh2_Canvas canvas, hh2_RGB565 color);
 
-hh2_Color* hh2_canvasPixel(hh2_Canvas canvas, unsigned x, unsigned y);
+hh2_RGB565* hh2_canvasPixel(hh2_Canvas canvas, unsigned x, unsigned y);
 
 #endif // HH2_CANVAS_H__
