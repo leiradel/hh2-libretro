ec1cbf9afb7531e2da9c6c757451513224c197db
Andre Leiradella <andre@leiradella.com>
2021-03-22 21:07:41 +0000

    Sprite refactor

--- src/engine/sprite.c
+++ src/engine/sprite.c
@@ -1,113 +1,118 @@
 #include "sprite.h"
+#include "log.h"
 
 #include <stdlib.h>
 
+#define TAG "SPT "
+
 #define HH2_MIN_SPRITES 64
+
+// The actual return value for createSprite is an uint16_t index into the sprites array, make sure we can store it in a pointer
+typedef char hh2_staticAssertSpeexSampleMustBeHh2Sample[sizeof(uint16_t) <= sizeof(hh2_Sprite) ? 1 : -1];
 
 typedef enum {
     HH2_SPRITE_INVISIBLE = 0x4000U,
-    HH2_SPRITE_UNUSED = 0x8000U,
-    HH2_SPRITE_FLAGS = HH2_SPRITE_INVISIBLE | HH2_SPRITE_UNUSED,
+    HH2_SPRITE_DESTROY = 0x8000U,
+    HH2_SPRITE_FLAGS = HH2_SPRITE_INVISIBLE | HH2_SPRITE_DESTROY,
     HH2_SPRITE_LAYER = ~HH2_SPRITE_FLAGS
 }
 hh2_SpriteFlags;
 
 struct hh2_Sprite {
-    uint16_t flags; // Sprite layer [0..16383] | visibility << 14 | << unused << 15
+    hh2_Image image;
+    hh2_RGB565* bg;
+
     int x;
     int y;
 
-    hh2_Image image;
-    hh2_RGB565* bg;
+    uint16_t flags; // Sprite layer [0..16383] | visibility << 14 | << unused << 15
 };
 
-static hh2_Sprite hh2_sprites;
+static hh2_Sprite* hh2_sprites = NULL;
 static size_t hh2_spriteCount = 0;
-static size_t hh2_spriteReserved = 0;
+static size_t hh2_reservedSprites = 0;
 
 hh2_Sprite hh2_createSprite(void) {
-    if (hh2_spriteCount < hh2_spriteReserved) {
-        hh2_Sprite const sprite = hh2_sprites + hh2_spriteCount;
+    HH2_LOG(HH2_LOG_INFO, TAG "creating sprite");
 
-        if ((sprite->flags & HH2_SPRITE_UNUSED) != 0) {
-            hh2_spriteCount++;
-            sprite->flags = 0;
-            sprite->bg = NULL;
-            return sprite;
+    hh2_Sprite sprite = (hh2_Sprite)malloc(sizeof(*sprite));
+
+    if (sprite == NULL) {
+        HH2_LOG(HH2_LOG_ERROR, TAG "out of memory");
+        return NULL;
+    }
+
+    if (hh2_spriteCount == hh2_reservedSprites) {
+        size_t const new_reserved = hh2_reservedSprites == 0 ? HH2_MIN_SPRITES : hh2_reservedSprites * 2;
+        HH2_LOG(HH2_LOG_DEBUG, TAG "sprite list full, growing from %zu to %zu", hh2_reservedSprites, new_reserved);
+
+        hh2_Sprite* const new_entries = realloc(hh2_sprites, sizeof(hh2_Sprite) * new_reserved);
+
+        if (new_entries == NULL) {
+            HH2_LOG(HH2_LOG_ERROR, TAG "out of memory");
+            free(sprite);
+            return NULL;
         }
+
+        hh2_reservedSprites = new_reserved;
+        hh2_sprites = new_entries;
     }
 
-    size_t const new_reserved = hh2_spriteReserved == 0 ? HH2_MIN_SPRITES : hh2_spriteReserved * 2;
-    hh2_Sprite const new_sprites = (hh2_Sprite)realloc(hh2_sprites, new_reserved * sizeof(*new_sprites));
+    hh2_sprites[hh2_spriteCount++] = sprite;
 
-    if (new_sprites != NULL) {
-        hh2_sprites = new_sprites;
-        hh2_spriteReserved = new_reserved;
-
-        hh2_Sprite const sprite = hh2_sprites + hh2_spriteCount++;
-        sprite->flags = 0;
-        sprite->bg = NULL;
-        return sprite;
-    }
-
-    return NULL;
+    HH2_LOG(HH2_LOG_DEBUG, TAG "created sprite %p", sprite);
+    return sprite;
 }
 
-void hh2_destroySprite(hh2_Sprite sprite) {
-    if (sprite->bg != NULL) {
-        free(sprite->bg);
-    }
-
-    sprite->flags = HH2_SPRITE_UNUSED;
+void hh2_destroySprite(hh2_Sprite const sprite) {
+    HH2_LOG(HH2_LOG_INFO, TAG "freeing sprite %p", sprite);
+    sprite->flags = HH2_SPRITE_DESTROY;
 }
 
-void hh2_setPosition(hh2_Sprite sprite, int x, int y) {
+void hh2_setPosition(hh2_Sprite const sprite, int x, int y) {
     sprite->x = x;
     sprite->y = y;
 }
 
-void hh2_setLayer(hh2_Sprite sprite, unsigned layer) {
+void hh2_setLayer(hh2_Sprite const sprite, unsigned const layer) {
     sprite->flags = (sprite->flags & HH2_SPRITE_FLAGS) | (layer & HH2_SPRITE_LAYER);
 }
 
-bool hh2_setImage(hh2_Sprite sprite, hh2_Image image) {
-    if (sprite->bg != NULL) {
-        free(sprite->bg);
-        sprite->bg = NULL;
-    }
+bool hh2_setImage(hh2_Sprite const sprite, hh2_Image const image) {
+    hh2_RGB565* bg = NULL;
 
     if (image != NULL) {
         size_t const count = hh2_changedPixels(image);
-        sprite->bg = (hh2_RGB565*)malloc(count * sizeof(*sprite->bg));
+        bg = (hh2_RGB565*)malloc(count * sizeof(*sprite->bg));
+
+        if (bg == NULL) {
+            HH2_LOG(HH2_LOG_ERROR, TAG "out of memory");
+            return false;
+        }
     }
 
+    free(sprite->bg);
+
     sprite->image = image;
-    return image != NULL && sprite->bg != NULL;
+    sprite->bg = bg;
+    return true;
 }
 
-void hh2_setVisibility(hh2_Sprite sprite, bool visible) {
+void hh2_setVisibility(hh2_Sprite const sprite, bool const visible) {
     sprite->flags = (HH2_SPRITE_INVISIBLE * !visible) | (sprite->flags & HH2_SPRITE_LAYER);
 }
 
 static int hh2_compareSprites(void const* e1, void const* e2) {
-    hh2_Sprite const s1 = (hh2_Sprite)e1;
-    hh2_Sprite const s2 = (hh2_Sprite)e2;
+    hh2_Sprite const s1 = *(hh2_Sprite*)e1;
+    hh2_Sprite const s2 = *(hh2_Sprite*)e2;
 
-    if (s1->flags == s2->flags) {
-        bool const i1 = s1->image != NULL;
-        bool const i2 = s2->image != NULL;
+    uint16_t const f1 = s1->flags | (HH2_SPRITE_INVISIBLE * (s1->image == NULL));
+    uint16_t const f2 = s2->flags | (HH2_SPRITE_INVISIBLE * (s2->image == NULL));
 
-        if (i1 == i2) {
-            return 0;
-        }
-        else if (i1 && !i2) {
-            return -1;
-        }
-        else {
-            return 1;
-        }
+    if (f1 == f2) {
+        return 0;
     }
-    else if (s1->flags < s2->flags) {
+    else if (f1 < f2) {
         return -1;
     }
     else {
@@ -117,25 +122,51 @@ static int hh2_compareSprites(void const* e1, void const* e2) {
 
 void hh2_blitSprites(hh2_Canvas const canvas) {
     qsort(hh2_sprites, hh2_spriteCount, sizeof(*hh2_sprites), hh2_compareSprites);
-    hh2_Sprite sprite = hh2_sprites;
 
-    if ((sprite->flags & HH2_SPRITE_INVISIBLE) == 0 && sprite->image != NULL) {
+    size_t i = 0;
+    hh2_Sprite sprite = hh2_sprites[0];
+
+    // Blit all sprites not invisible and not marked for destruction
+    if (i < hh2_spriteCount && (sprite->flags & HH2_SPRITE_FLAGS) == 0 && sprite->image != NULL) {
         do {
             hh2_blit(sprite->image, canvas, sprite->x, sprite->y, sprite->bg);
-            sprite++;
+            sprite = hh2_sprites[++i];
         }
-        while ((sprite->flags & HH2_SPRITE_INVISIBLE) == 0 && sprite->image != NULL);
+        while (i < hh2_spriteCount && (sprite->flags & HH2_SPRITE_FLAGS) == 0 && sprite->image != NULL);
     }
+
+    // Skip invisible sprites
+    if (i < hh2_spriteCount && ((sprite->flags & HH2_SPRITE_FLAGS) == HH2_SPRITE_INVISIBLE || sprite->image == NULL)) {
+        do {
+            sprite = hh2_sprites[++i];
+        }
+        while (i < hh2_spriteCount && ((sprite->flags & HH2_SPRITE_FLAGS) == HH2_SPRITE_INVISIBLE || sprite->image == NULL));
+    }
+
+    // Destroy all remaining sprites
+    size_t const new_count = i;
+
+    if (i < hh2_spriteCount) {
+        do {
+            free(sprite->bg);
+            free(sprite);
+            sprite = hh2_sprites[++i];
+        }
+        while (i < hh2_spriteCount);
+    }
+
+    hh2_spriteCount = new_count;
 }
 
 void hh2_unblitSprites(hh2_Canvas const canvas) {
-    hh2_Sprite sprite = hh2_sprites;
+    size_t i = 0;
+    hh2_Sprite sprite = hh2_sprites[0];
 
-    if ((sprite->flags & HH2_SPRITE_INVISIBLE) == 0 && sprite->image != NULL) {
+    if (i < hh2_spriteCount) {
         do {
             hh2_unblit(sprite->image, canvas, sprite->x, sprite->y, sprite->bg);
-            sprite++;
+            sprite = hh2_sprites[++i];
         }
-        while ((sprite->flags & HH2_SPRITE_INVISIBLE) == 0 && sprite->image != NULL);
+        while (i < hh2_spriteCount);
     }
 }
