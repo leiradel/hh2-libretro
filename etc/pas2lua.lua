57430da0c3ef6904f52f406c38a04f46475d0573
Andre Leiradella <andre@leiradella.com>
2021-03-28 12:50:22 +0100

    Revert "Removed debug print"
    This reverts commit 3a569687afa0ff55c23ae9ba43e0f8dc636accbb.

--- etc/pas2lua.lua
+++ etc/pas2lua.lua
@@ -10,6 +10,7 @@ end
 
 local function append(dest, source)
     for i = 1, #source do
+        --print(string.format('dest[%d] = source[%d]', #dest + 1, i))
         dest[#dest + 1] = source[i]
     end
 end
@@ -287,8 +288,9 @@ local function newParser(path)
             'longint', 'int64', 'word', 'boolean', 'char', 'widechar', 'longword', 'pchar',
             'div', 'mod', 'and', 'or', 'in', 'is',
             'unit', 'interface', 'uses', 'type', 'true', 'false', 'class', 'end', 'procedure', 'function', 'var', 'const', 'array',
-            'initialization', 'of', 'record', 'implementation', 'begin', 'not', 'if', 'then', 'else', 'for', 'to', 'downto', 'do',
-            'while', 'case'
+            'initialization',
+            'of', 'record', 'implementation', 'begin', 'not',
+            'if', 'then', 'else', 'for', 'to', 'downto', 'do', 'while', 'case'
         }
     }
 
@@ -1259,11 +1261,5 @@ local function newParser(path)
     return parser
 end
 
-local function generate(ast)
-
-end
-
 local parser = newParser('games/popeye/unit1.pas')
-local ast = parser:parse()
-dump(ast)
-generate(ast)
+parser:parse()
