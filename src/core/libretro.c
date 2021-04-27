7709eac1393d7d3e385f86c46fe3203d66bf0337
Andre Leiradella <andre@leiradella.com>
2021-04-27 19:25:36 +0100

    Started integrating the actual runtime into the Libretro core

--- src/core/libretro.c
+++ src/core/libretro.c
@@ -1,12 +1,8 @@
 #include "libretro.h"
 
-#include "canvas.h"
 #include "filesys.h"
-#include "image.h"
 #include "log.h"
-#include "pixelsrc.h"
-#include "sound.h"
-#include "sprite.h"
+#include "state.h"
 #include "version.h"
 
 #include <stdio.h>
@@ -15,50 +11,21 @@
 
 #define TAG "COR "
 
-// Do not change these values!
-#define HH2C_SRAM_MAX_ENTRIES 8
-#define HH2C_SRAM_MAX_KEY_LENGTH 32
-#define HH2C_SRAM_MAX_VALUE_LENGTH 64
-
-typedef struct {
-    // 777 bytes for the registry
-    uint8_t types[HH2C_SRAM_MAX_ENTRIES];
-    uint8_t keys[HH2C_SRAM_MAX_ENTRIES][HH2C_SRAM_MAX_KEY_LENGTH];
-    uint8_t values[HH2C_SRAM_MAX_ENTRIES][HH2C_SRAM_MAX_VALUE_LENGTH];
-    uint8_t count;
-
-    // Pad to next multiple of four
-    uint8_t padding[3];
-
-    // Fake SRAM for achievements, all values are guaranteed to be written in little-endian
-    uint32_t sram[317];
-}
-hh2c_Sram;
-
-// Make sure hh2_Sram has 2048 bytes
-typedef char hh2c_staticAssertSramMustHave2048Bytes[sizeof(hh2c_Sram) == 2048 ? 1 : -1];
-
 // Libretro callbacks
-static retro_environment_t hh2c_environment_cb;
-static retro_log_printf_t hh2c_log_printf_cb;
-static retro_input_poll_t hh2c_input_poll_cb;
-static retro_input_state_t hh2c_input_state_cb;
-static retro_video_refresh_t hh2c_video_refresh_cb;
-static retro_audio_sample_batch_t hh2c_audio_sample_batch_cb;
+static retro_environment_t environment_cb;
+static retro_log_printf_t log_printf_cb;
+static retro_input_poll_t input_poll_cb;
+static retro_input_state_t input_state_cb;
+static retro_video_refresh_t video_refresh_cb;
+static retro_audio_sample_batch_t audio_sample_batch_cb;
 
 // hh2 globals
-static void* hh2c_content;
-static hh2_Filesys hh2c_filesys;
-static hh2_Canvas hh2c_canvas;
-static hh2_Image hh2c_image;
-static hh2_Sprite hh2c_sprite;
-static int hh2c_x, hh2c_y, hh2c_dx, hh2c_dy;
-static bool hh2c_unblit;
-static hh2_Pcm hh2c_pcm;
-static unsigned hh2c_frame;
+static void* content;
+static hh2_Filesys filesys;
+static hh2_State state;
 
 // The logger function to hh2_setLogger
-static void hh2c_logger(hh2_LogLevel const level, char const* const format, va_list ap) {
+static void logger(hh2_LogLevel const level, char const* const format, va_list ap) {
     enum retro_log_level lr_level = RETRO_LOG_ERROR;
 
     switch (level) {
@@ -68,9 +35,9 @@ static void hh2c_logger(hh2_LogLevel const level, char const* const format, va_l
         default: break;
     }
 
-    char message[256];
+    char message[4096];
     vsnprintf(message, sizeof(message), format, ap);
-    hh2c_log_printf_cb(lr_level, "%s\n", message);
+    log_printf_cb(lr_level, "%s\n", message);
 }
 
 // Libretro API implementation
@@ -83,13 +50,13 @@ void retro_get_system_info(struct retro_system_info* const info) {
 }
 
 void retro_set_environment(retro_environment_t const cb) {
-    hh2c_environment_cb = cb;
+    environment_cb = cb;
 
     struct retro_log_callback log;
 
     if (cb(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &log)) {
-        hh2c_log_printf_cb = log.log;
-        hh2_setLogger(hh2c_logger);
+        log_printf_cb = log.log;
+        hh2_setLogger(logger);
     }
 }
 
@@ -102,15 +69,15 @@ void retro_init() {
 }
 
 void retro_set_input_poll(retro_input_poll_t const cb) {
-    hh2c_input_poll_cb = cb;
+    input_poll_cb = cb;
 }
 
 void retro_set_input_state(retro_input_state_t const cb) {
-    hh2c_input_state_cb = cb;
+    input_state_cb = cb;
 }
 
 void retro_set_video_refresh(retro_video_refresh_t const cb) {
-    hh2c_video_refresh_cb = cb;
+    video_refresh_cb = cb;
 }
 
 void retro_set_audio_sample(retro_audio_sample_t const cb) {
@@ -118,7 +85,7 @@ void retro_set_audio_sample(retro_audio_sample_t const cb) {
 }
 
 void retro_set_audio_sample_batch(retro_audio_sample_batch_t const cb) {
-    hh2c_audio_sample_batch_cb = cb;
+    audio_sample_batch_cb = cb;
 }
 
 bool retro_load_game(struct retro_game_info const* const info) {
@@ -129,76 +96,31 @@ bool retro_load_game(struct retro_game_info const* const info) {
 
     enum retro_pixel_format pixel_format = RETRO_PIXEL_FORMAT_RGB565;
 
-    if (!hh2c_environment_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &pixel_format)) {
+    if (!environment_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &pixel_format)) {
         HH2_LOG(HH2_LOG_ERROR, TAG "front-end does not support RGB565");
         return false;
     }
 
-    hh2c_content = malloc(info->size);
+    content = malloc(info->size);
 
-    if (hh2c_content == NULL) {
+    if (content == NULL) {
         HH2_LOG(HH2_LOG_ERROR, TAG "out of memory");
         return false;
     }
 
-    memcpy(hh2c_content, info->data, info->size);
-    hh2c_filesys = hh2_createFilesystem(hh2c_content, info->size);
+    memcpy(content, info->data, info->size);
+    filesys = hh2_createFilesystem(content, info->size);
 
-    if (hh2c_filesys == NULL) {
+    if (filesys == NULL) {
         // Error already logged
-        free(hh2c_content);
+        free(content);
         return false;
     }
 
-    hh2_PixelSource const source = hh2_readPixelSource(hh2c_filesys, "test/cryptopunk32.png");
-
-    if (source == NULL) {
+    if (!hh2_initState(&state, filesys)) {
         // Error already logged
-        hh2_destroyFilesystem(hh2c_filesys);
-        free(hh2c_content);
-        return false;
-    }
-
-    hh2c_image = hh2_createImage(source);
-
-    if (hh2c_image == NULL) {
-        // Error already logged
-        hh2_destroyPixelSource(source);
-        hh2_destroyFilesystem(hh2c_filesys);
-        free(hh2c_content);
-        return false;
-    }
-
-    hh2_destroyPixelSource(source);
-    hh2c_sprite = hh2_createSprite();
-
-    if (hh2c_sprite == NULL) {
-        // error already logged
-        hh2_destroyImage(hh2c_image);
-        hh2_destroyFilesystem(hh2c_filesys);
-        free(hh2c_content);
-        return false;
-    }
-
-    hh2_setLayer(hh2c_sprite, 0);
-    hh2_setImage(hh2c_sprite, hh2c_image);
-    hh2_setVisibility(hh2c_sprite, true);
-
-    hh2c_canvas = hh2_createCanvas(256, 192);
-    hh2_clearCanvas(hh2c_canvas, HH2_COLOR_RGB565(0x84, 0xb0, 0x95));
-
-    hh2c_x = hh2c_y = 0;
-    hh2c_dx = hh2c_dy = 1;
-    hh2c_unblit = false;
-
-    hh2c_pcm = hh2_readPcm(hh2c_filesys, "test/tick.wav");
-
-    if (hh2c_pcm == NULL) {
-        // error already logged
-        hh2_destroyCanvas(hh2c_canvas);
-        hh2_destroyImage(hh2c_image);
-        hh2_destroyFilesystem(hh2c_filesys);
-        free(hh2c_content);
+        hh2_destroyFilesystem(filesys);
+        free(content);
         return false;
     }
 
@@ -229,48 +151,22 @@ unsigned retro_get_region() {
 }
 
 size_t retro_get_memory_size(unsigned id) {
-    return 0; //id == RETRO_MEMORY_SAVE_RAM ? sizeof(hh2c_state.sram) : 0;
+    return id == RETRO_MEMORY_SAVE_RAM ? sizeof(state.sram) : 0;
 }
 
 void* retro_get_memory_data(unsigned id) {
-    return NULL; //id == RETRO_MEMORY_SAVE_RAM ? &hh2c_state.sram : NULL;
+    return id == RETRO_MEMORY_SAVE_RAM ? &state.sram : NULL;
 }
 
 void retro_run() {
-    if (hh2c_unblit) {
-        hh2_unblitSprites(hh2c_canvas);
-    }
-
-    hh2c_x += hh2c_dx;
-
-    if (hh2c_x < -24 || hh2c_x > 256) {
-        hh2c_dx = -hh2c_dx;
-    }
-
-    hh2c_y += hh2c_dy;
-
-    if (hh2c_y < -24 || hh2c_y > 192) {
-        hh2c_dy = -hh2c_dy;
-    }
-
-    hh2c_input_poll_cb();
-
-    hh2_setPosition(hh2c_sprite, hh2c_x, hh2c_y);
-
-    hh2_blitSprites(hh2c_canvas);
-    hh2c_unblit = true;
-
-    if (((hh2c_frame++) & 127) == 0) {
-        hh2_playPcm(hh2c_pcm);
-    }
-
-    hh2_RGB565 const* const framebuffer = hh2_canvasPixel(hh2c_canvas, 0, 0);
+    /*hh2_RGB565 const* const framebuffer = hh2_canvasPixel(hh2c_canvas, 0, 0);
     size_t const pitch = hh2_canvasPitch(hh2c_canvas);
     hh2c_video_refresh_cb(framebuffer, 256, 192, pitch);
 
     size_t frames;
     int16_t const* samples = hh2_soundMix(&frames);
-    hh2c_audio_sample_batch_cb(samples, frames);
+    hh2c_audio_sample_batch_cb(samples, frames);*/
+    video_refresh_cb(NULL, 0, 0, 0);
 }
 
 void retro_set_controller_port_device(unsigned const port, unsigned const device) {
@@ -303,11 +199,9 @@ void retro_cheat_set(unsigned const a, bool const b, char const* const c) {
 }
 
 void retro_unload_game() {
-    hh2_destroyPcm(hh2c_pcm);
-    hh2_destroyCanvas(hh2c_canvas);
-    hh2_destroyImage(hh2c_image);
-    hh2_destroyFilesystem(hh2c_filesys);
-    free(hh2c_content);
+    hh2_destroyFilesystem(filesys);
+    free(content);
+    hh2_destroyState(&state);
 }
 
 void retro_deinit() {}
