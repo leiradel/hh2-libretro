4ed7e3f73e646bb336e679f31d8bb4bc7139f553
Andre Leiradella <andre@leiradella.com>
2021-05-03 22:48:24 +0100

    boot.js is compressed

--- src/runtime/bootstrap.js
+++ src/runtime/bootstrap.js
@@ -1,5 +1,5 @@
 function(hh2) {
-    const bootSource = hh2.load("boot.js");
+    const bootSource = hh2.load("boot.js.gz");
     const bootFunction = hh2.compile(bootSource, "boot.js");
     bootFunction(hh2);
 }
