3a53a3d0c770e0a33107c8861492aae6c50c212d
Andre Leiradella <andre@leiradella.com>
2021-05-03 22:50:48 +0100

    Also allow programs in addition to units in extmodule

--- etc/extmodule.lua
+++ etc/extmodule.lua
@@ -1,10 +1,20 @@
 if #arg ~= 1 then
-    io.stderr:write('Usage: lua extmodule.lua <modulename>')
+    io.stderr:write('Usage: lua extmodule.lua <modulename>\n')
     os.exit(1)
 end
 
 local source = io.stdin:read('*a')
 local start = source:find(string.format('rtl.module("%s"', arg[1]), 1, true)
+
+if not start then
+    start = source:find('rtl.module("program"', 1, true)
+
+    if not start then
+        io.stderr:write(string.format('Could not find module "%s"\n', arg[1]))
+        os.exit(1)
+    end
+end
+
 local finish = source:find('rtl.module("', start + 1, true)
 
 if not finish then
