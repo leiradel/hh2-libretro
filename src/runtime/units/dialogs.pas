4418a37b0cb74a21af46983346004c95138da874
Andre Leiradella <andre@leiradella.com>
2021-06-20 18:08:59 +0100

    Added missing elements to TMsgDlgBtn

--- src/runtime/units/dialogs.pas
+++ src/runtime/units/dialogs.pas
@@ -4,7 +4,7 @@ interface
 
 type
     TMsgDlgType = (mtWarning, mtError, mtInformation, mtConfirmation, mtCustom);
-    TMsgDlgBtn = (mbYes, mbNo, mbOK, mbCancel, mbAbort, mbRetry, mbIgnore, mbAll, mbNoToAll, mbYesToAll, mbHelp, mbClose);
+    TMsgDlgBtn = (mbYes, mbNo, mbOK, mbCancel, mbAbort, mbRetry, mbIgnore, mbAll, mbNoToAll, mbYesToAll, mbHelp, mbClose, mbYesNoCancel, mbYesAllNoAllCancel, mbOKCancel, mbAbortRetryIgnore, mbAbortIgnore);
     TMsgDlgButtons = set of TMsgDlgBtn;
 
 function MessageDlg(const Msg: String; DlgType: TMsgDlgType; Buttons: TMsgDlgButtons; HelpCtx: Longint): Integer;
