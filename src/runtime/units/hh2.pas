9e425fda218420f2a891c5f0a22c24b17ca7953f
Andre Leiradella <andre@leiradella.com>
2021-07-04 20:52:25 +0100

    Implemented procedures in "assembly"

--- src/runtime/units/hh2.pas
+++ src/runtime/units/hh2.pas
@@ -8,12 +8,47 @@ uses
 type
     RetropadButton = (Up, Down, Left, Right, A, B, X, Y, L, R, L2, R2, L3, R3, Select, Start);
 
-procedure SetBackground(Image: TImage); external name 'hh2.setBackground';
-procedure AddGameScreen(Left, Top, Right, Bottom: Integer); external name 'hh2.addGameScreen';
-procedure MapButton(Image: TImage; Button: RetropadButton); external name 'hh2.mapButton';
-procedure MapTouch(Image: TImage; Left, Top, Right, Bottom: Integer; Caption: String); external name 'hh2.mapTouch';
-procedure AddUnmappedButton(Image: TImage; Caption: String); external name 'hh2.AddUnmappedButton';
+procedure SetBackground(Image: TImage);
+procedure AddGameScreen(Left, Top, Right, Bottom: Integer);
+procedure MapButton(Image: TImage; Button: RetropadButton);
+procedure MapTouch(Image: TImage; Left, Top, Right, Bottom: Integer; Caption: String);
+procedure AddUnmappedButton(Image: TImage; Caption: String);
 
 implementation
+
+procedure SetBackground(Image: TImage);
+begin
+    asm
+        hh2rt.setBackground(image);
+    end;
+end;
+
+procedure AddGameScreen(Left, Top, Right, Bottom: Integer);
+begin
+    asm
+        hh2rt.addGameScreen(left, top, right, bottom);
+    end;
+end;
+
+procedure MapButton(Image: TImage; Button: RetropadButton);
+begin
+    asm
+        hh2rt.mapButton(image, button);
+    end;
+end;
+
+procedure MapTouch(Image: TImage; Left, Top, Right, Bottom: Integer; Caption: String);
+begin
+    asm
+        hh2rt.mapTouch(image, left, top, right, bottom, caption);
+    end;
+end;
+
+procedure AddUnmappedButton(Image: TImage; Caption: String);
+begin
+    asm
+        hh2rt.addUnmappedButton(image, caption);
+    end;
+end;
 
 end.
