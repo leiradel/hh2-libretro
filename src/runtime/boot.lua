a4732559d9b03dd577bd35e1afc956e6289c06df
Andre Leiradella <andre@leiradella.com>
2021-07-22 18:46:47 +0100

    Started showing stuff on the screen

--- src/runtime/boot.lua
+++ src/runtime/boot.lua
@@ -1,7 +1,15 @@
-return function()
+local hh2rt = require 'hh2rt'
 
+return function()
     local main = require 'hh2main'
     main.initgame()
 
     local config = require 'hh2config'
+
+    local background = hh2rt.readPixelSource(config.background_image)
+    local image = hh2rt.createImage(background)
+
+    hh2rt.createCanvas(background:width(), background:height())
+
+    image:stamp(0, 0)
 end
