f7b475c7ff163b2d4705d68ef61c57ef50631f7b
Andre Leiradella <andre@leiradella.com>
2021-07-25 17:59:25 +0100

    Return the tick function

--- src/runtime/boot.lua
+++ src/runtime/boot.lua
@@ -1,15 +1,35 @@
 local hh2rt = require 'hh2rt'
 
 return function()
+    -- Initialize the game
     local main = require 'hh2main'
     main.initgame()
 
+    -- Set the background image for the game
     local config = require 'hh2config'
 
     local background = hh2rt.readPixelSource(config.background_image)
     local image = hh2rt.createImage(background)
 
     hh2rt.createCanvas(background:width(), background:height())
-
     image:stamp(0, 0)
+
+    -- Return the tick function
+    return function()
+        local now = hh2rt.now()
+        hh2rt.debug('now is %d', now)
+
+        for timer in pairs(hh2rt.timers) do
+            hh2rt.debug('%s %s %s %s', timer.enabled, timer.ontimer, timer.interval, timer.expiration)
+            if timer.enabled and timer.ontimer and timer.interval ~= 0 and now >= timer.expiration then
+                timer.ontimer()
+            end
+        end
+
+        for image in pairs(hh2rt.images) do
+            image.sprite:setImage(image.picture.image)
+            image.sprite:setPosition(image.left, image.top)
+            image.sprite:setVisibility(iamge.visible)
+        end
+    end
 end
