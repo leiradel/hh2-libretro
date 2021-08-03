169416e09541f18eaed28a505e2f1787f7216ea8
Andre Leiradella <andre@leiradella.com>
2021-08-03 21:13:31 +0100

    Expose the script that generates stuff from the games

---
+++ etc/generate.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+
+generate() {
+    LUA_PATH="$LUAMODS/access/src/?.lua;$LUAMODS/inifile/src/?.lua" \
+    LUA_CPATH="$LUAMODS/proxyud/src/?.so;$LUAMODS/ddlt/?.so" \
+    lua $BASEDIR/packgame.lua --settings $1 > $1/hh2config.lua
+
+    LUA_PATH="$LUAMODS/access/src/?.lua;$LUAMODS/inifile/src/?.lua;../etc/?.lua" \
+    LUA_CPATH="$LUAMODS/proxyud/src/?.so;$LUAMODS/ddlt/?.so" \
+    lua $BASEDIR/dfm2pas.lua $1/unit1.txt > $1/hh2dfm.pas
+
+    LUA_PATH="$LUAMODS/access/src/?.lua;$LUAMODS/inifile/src/?.lua;../etc/?.lua" \
+    LUA_CPATH="$LUAMODS/proxyud/src/?.so;$LUAMODS/ddlt/?.so" \
+    lua $BASEDIR/packgame.lua --makefile $1 > Makefile.$1
+
+    LUA_PATH="$LUAMODS/access/src/?.lua;$LUAMODS/inifile/src/?.lua;../etc/?.lua" \
+    LUA_CPATH="$LUAMODS/proxyud/src/?.so;$LUAMODS/ddlt/?.so" \
+    lua $BASEDIR/packgame.lua --gfxinit $1 > gfxinit.$1.pas
+}
+
+generate popeye
