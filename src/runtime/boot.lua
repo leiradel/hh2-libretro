4ff0828b2f438bca697237ab2d792e62e0745193
leiradel <andre@leiradella.com>
2021-12-01 16:19:43 +0000

    Configure controller buttons for all the controller profiles

--- src/runtime/boot.lua
+++ src/runtime/boot.lua
@@ -14,10 +14,36 @@ return function()
     background:stamp(0, 0)
 
     -- Configure the buttons depending on the profile
-    if config.mappingProfile == 'leftright' then
-        config.mappedButtons.a = config.mappedButtons.right
-    elseif config.mappingProfile == 'dpadaction' then
+    if config.mappingProfile == 'dpadaction' then
         config.mappedButtons.b = config.mappedButtons.action
+    elseif config.mappingProfile == 'dpadtwoactions' then
+        config.mappedButtons.b = config.mappedButtons.action1
+        config.mappedButtons.a = config.mappedButtons.action2
+    elseif config.mappingProfile == 'leftright' then
+        config.mappedButtons.a = config.mappedButtons.right
+    elseif config.mappingProfile == 'leftrightaction' then
+        config.mappedButtons.a = config.mappedButtons.right
+        config.mappedButtons.b = config.mappedButtons.action
+    elseif config.mappingProfile == 'updownaction' then
+        config.mappedButtons.b = config.mappedButtons.action
+    elseif config.mappingProfile == 'fourdiagonals' then
+        config.mappedButtons.up = config.mappedButtons.nw
+        config.mappedButtons.down = config.mappedButtons.sw
+        config.mappedButtons.x = config.mappedButtons.ne
+        config.mappedButtons.b = config.mappedButtons.se
+    elseif config.mappingProfile == 'threehorizontal' then
+        -- unsure
+    elseif config.mappingProfile == 'updownleftright' then
+        config.mappedButtons.y = config.mappedButtons.left
+        config.mappedButtons.a = config.mappedButtons.right
+        config.mappedButtons.left = nil
+        config.mappedButtons.right = nil
+    elseif config.mappingProfile == 'leftrightaction2p' then
+        config.mappedButtons.b = config.mappedButtons.actionp1
+        config.mappedButtons['b/2'] = config.mappedButtons.actionp2
+    elseif config.mappingProfile == 'leftrighttwoactions' then
+        config.mappedButtons.y = config.mappedButtons.actionleft
+        config.mappedButtons.a = config.mappedButtons.actionright
     else
         hh2rt.warn('unknown mapping profile: %s', config.mappingProfile)
     end
